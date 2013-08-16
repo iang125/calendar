654719c01148d1db321ec747d420d3ba974ad366
iang125 <RubyRailsIang@gmail.com>
2013-08-16 18:52:01 -0500

    added bcrypt and modified user form accordingly

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,4 @@
 class Question < ActiveRecord::Base
   belongs_to :event
   belongs_to :response
-
-  if self.response.atending = false
-    self.delete
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :responses
   has_many :questions, :through => :responses
   has_many :events, :through => :questions
+
+  has_secure_password
+
+  validates :email, :presence => true, :uniqueness => true
 end
