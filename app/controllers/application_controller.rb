684942c6d5f6b5510bb3693194744d26bae18f36
iang125 <RubyRailsIang@gmail.com>
2013-08-16 18:42:41 -0500

    added current_user to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+   before_action :log_session_data
+
+  def log_session_data
+    session.keys.each do |k|
+      logger.info "Session key #{k}: #{session[k]}"
+    end
+  end
+
+  def current_user
+    return User.find_by(:id => session[:user_id])
+  end
+
+  helper_method :current_user
 end
